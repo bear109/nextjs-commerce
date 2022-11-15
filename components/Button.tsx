1747a5e17cf62d6a1e186c6192d60e986834ff6f
bear109 <jokerbear109@gmail.com>
2022-11-15 21:57:07 +0900

    feat: emotion 적용

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import styled from '@emotion/styled';
 
-export default function Button() {
-  return <div>Button</div>;
-}
+const Button = styled.button`
+  padding: 16px;
+  border-radius: 8px;
+  background-color: pink;
+`;
+export default Button;
